b6ce535f600f8d40018c9c115f9b77d6c83529f9
panjigemilang <41560479+panjigemilang@users.noreply.github.com>
2020-06-22 22:26:13 +0700

    Landing Page. haven't add the side nav. Beta_v.1.0.0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense } from "react"
+import "./App.css"
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Utils
+import { lightTheme, darkTheme } from "./Components/utils/Styled"
+import { GlobalStyles } from "./Components/utils/GlobalStyles"
+import Loading from "./Components/utils/Loading"
+
+// Layouts
+import Navbar from "./Components/layouts/Navbar"
+import Footer from "./Components/layouts/Footer"
+
+// Components
+const Landing = lazy(() => import("./Components/pages/Landing"))
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Suspense fallback={<Loading />}>
+          <ThemeProvider theme={lightTheme}>
+            <GlobalStyles />
+            <Navbar />
+            <Route component={Landing} />
+            <Footer />
+          </ThemeProvider>
+        </Suspense>
+      </Router>
+    )
+  }
 }
-
-export default App;
